f4dea89920b8075d93930d031f31a3694d54fe8d
minguni <80194344+min-guni@users.noreply.github.com>
2026-07-14 16:19:18 +0900

    build: 멀티모듈 Gradle 골격

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+rootProject.name = "alphatalk"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
+
+include("contracts", "auth-jwt", "ws")
